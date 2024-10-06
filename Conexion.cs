f6f8b66b663c6180eea72d159b9b27b90708cf50
silvana <silvanalorena624@gmail.com>
2024-10-06 17:01:13 -0300

    Agregar archivos de proyecto.

---
+++ Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DSOO_PI1_ComB_Grupo15_Paez_Fernandez
+{
+    internal class Conexion
+    {
+
+    }
+}
