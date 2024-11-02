fa11f03a0e6cc026c8a7d5002165aaab1863d1e9
silvana <silvanalorena624@gmail.com>
2024-11-02 15:32:39 -0300

    cambios en la clase conexión

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 
 namespace DSOO_PI1_ComB_Grupo15_Paez_Fernandez.Datos
 {
-    internal class Conexion
+    public class Conexion
     {
         private string baseDatos;
         private string servidor;
@@ -19,22 +19,58 @@ namespace DSOO_PI1_ComB_Grupo15_Paez_Fernandez.Datos
 
         private Conexion()
         {
-            baseDatos = "DSOO_PI1_ComB_Grupo15_Paez_Fernandez";
-            servidor = "localhost";
-            puerto = "3306";
-            usuario = "root";
-            clave = "Posadas3213";
+            // variables usadas para larepetición de líneas de código
+            bool correcto = false;
+            int mensaje;
+
+            // creamos las variables para recibir los datos desde el teclado
+            string T_servidor = "Servidor";
+            string T_puerto = "Puerto";
+            string T_usuario = "Usuario";
+            string T_clave = "Clave";
+
+            while (correcto != true)
+            {
+                // Armamos los cuadros de dialogo para el ingreso de datos
+                T_servidor = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese servidor", "DATOS DE INSTALACIÓN MySQL");
+                T_puerto = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese puerto", "DATOS DE INSTALACIÓN MySQL");
+                T_usuario = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese usuario", "DATOS DE INSTALACIÓN MySQL");
+                T_clave = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese clave", "DATOS DE INSTALACIÓN MySQL");
+
+                mensaje = (int)MessageBox.Show("su ingreso: SERVIDOR = " + T_servidor + " PUERTO= " + T_puerto + " USUARIO: " + T_usuario + " CLAVE: " + T_clave, "AVISO DEL SISTEMA", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (mensaje != 6) // el valor 6 corresponde al SI
+                {
+                    MessageBox.Show("INGRESE NUEVAMENTE LOS DATOS");
+                    correcto = false;
+                }
+                else
+                {
+                    correcto = true;
+                }
+            }
+
+            this.baseDatos = "DSOO_PI1_ComB_Grupo15_Paez_Fernandez";
+            this.servidor = T_servidor;
+            this.puerto = T_puerto;
+            this.usuario = T_usuario;
+            this.clave = T_clave;
         }
 
+            //procedimiento de conexión
         public MySqlConnection CrearConexion()
         {
             MySqlConnection cadena = new MySqlConnection();
 
             try
             {
-                cadena.ConnectionString = "datasource=" + servidor +
-                    ";port=" + puerto + ";username=" + usuario + ";password=" +
-                    clave + ";Database=" + baseDatos;
+                cadena.ConnectionString = "datasource=" + this.servidor +
+                    ";port=" + this.puerto + ";username=" + this.usuario + ";password=" +
+                    this.clave + ";Database=" + baseDatos;
             }
             catch (Exception ex)
             {
@@ -46,11 +82,12 @@ namespace DSOO_PI1_ComB_Grupo15_Paez_Fernandez.Datos
         
         public static Conexion getInstancia()
         {
-            if (con == null)
+            if (con == null)// si la conexión está cerrada
             {
-                con = new Conexion();
+                con = new Conexion();//se crea una conexión
             }
             return con;
         }
     }
 }
+ 
